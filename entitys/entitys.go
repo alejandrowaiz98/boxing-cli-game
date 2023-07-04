3a5396f3d7441509323834e5f513e9dc677e10be
Alejandro Waiz <alejosaurio1@gmail.com>
2023-07-04 19:58:00 -0400

    Added entitys creation logic

--- entitys/entitys.go
+++ entitys/entitys.go
@@ -3,3 +3,25 @@ package entitys
 import "github.com/alejandrowaiz98/boxing-cli-game/config"
 
 var logger = config.GetLogger()
+
+type Entitys struct{}
+
+func (e *Entitys) Init() error {
+
+	err := createPunches()
+
+	if err != nil {
+		logger.Err(err).Msgf("initEntitys | createPunches: %v", err)
+		return err
+	}
+
+	err = createTournaments()
+
+	if err != nil {
+		logger.Err(err).Msgf("initEntitys | createTournaments: %v", err)
+		return err
+	}
+
+	return nil
+
+}
